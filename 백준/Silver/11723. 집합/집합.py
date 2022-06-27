435f187f44c36b6a00fd7776ffa186265be5a4a3
tjwjdrok <42794396+seojeongrok@users.noreply.github.com>
2022-06-27 18:48:54 +0900

    Update 집합.py

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,3 +1,12 @@
+# input() vs sys.stdin.readline() -> sys.stdin.readline()이 시간이 짧음
+# input(메시지) -> 메시지를 우리는 '프롬프트 메시지'라고 한다.
+# 파이썬은 입력을 받을 때 개행 문자(\n)이 딸려온다. input()은 개형 문자를 떼버리는데 sys.stdin.readline()은 개행문자를 안 떼고 그냥 받는다.
+# sys.stdin.readline()으로 받고 나면 항상 strip()이나 rstrip() 메소드가 따라와야 한다.
+
+# sys.stdin.readline()이 빠른 이유
+# 1. 개행문자를 안 떼고
+# 2. 프롬프트 메시지도 안 받기 때문
+
 import sys
 m = int(sys.stdin.readline())
 S = set()
