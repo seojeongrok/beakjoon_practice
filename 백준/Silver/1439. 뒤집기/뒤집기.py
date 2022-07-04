14c305a33b558f8da1b6a5c6d44f925d909fbcfd
tjwjdrok <42794396+seojeongrok@users.noreply.github.com>
2022-07-04 20:45:39 +0900

    [Silver V] Title: 뒤집기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -0,0 +1,14 @@
+zero_cnt, one_cnt = 0, 0
+
+s = input()
+if s[-1] == '1':
+    one_cnt += 1
+if s[-1] == '0':
+    zero_cnt += 1
+
+for i in range(len(s)-1):
+    if s[i] == '0' and s[i] != s[i+1]:
+        zero_cnt += 1
+    if s[i] == '1' and s[i] != s[i+1]:
+        one_cnt += 1
+print(min(zero_cnt, one_cnt))
