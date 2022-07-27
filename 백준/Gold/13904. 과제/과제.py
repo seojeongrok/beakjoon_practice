8b6841b5aae745d4d2fa386542b17c1e26890382
tjwjdrok <42794396+seojeongrok@users.noreply.github.com>
2022-07-27 11:25:56 +0900

    Update 과제.py

--- 백준/Gold/13904. 과제/과제.py
+++ 백준/Gold/13904. 과제/과제.py
@@ -1,16 +1,27 @@
 n = int(input())
 work = []
-day = [0] * 1000
 
 for i in range(n):
     d, w = map(int, input().split())
     work.append([d, w])
 
-work.sort(reverse=True, key=lambda x : x[1])
+# 점수순으로 내림차순 정렬
+work.sort(reverse=True, key=lambda x: x[1])
 
-for i in range(n):
-    for j in range(work[i][0]-1, -1 ,-1):
-        if day[j] == 0:
-            day[j] = work[i][1]
+day = {}
+
+for date, score in work:
+    tmp = date
+    while tmp >= 1:
+        if tmp not in day:
+            day[tmp] = score
             break
-print(sum(day))
+        else:
+            tmp -= 1
+
+result = 0
+
+for i in day.values():
+    result += i
+
+print(result)
