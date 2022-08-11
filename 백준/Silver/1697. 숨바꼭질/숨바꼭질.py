d37d0ed12ebe866cc1eaba73472e6de19d414f2c
tjwjdrok <42794396+seojeongrok@users.noreply.github.com>
2022-08-11 12:19:07 +0900

    [Silver I] Title: 숨바꼭질, Time: 176 ms, Memory: 35200 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,16 @@
+from collections import deque
+n, k = map(int, input().split())
+MAX = 10 ** 5
+dis = [0] * (MAX + 1)
+
+q = deque()
+q.append(n)
+while q:
+    x = q.popleft()
+    if x == k:
+        print(dis[x])
+        break
+    for tmp in (x-1, x+1, x*2):
+        if 0 <= tmp <= MAX and not dis[tmp]:
+            dis[tmp] = dis[x] + 1
+            q.append(tmp)
