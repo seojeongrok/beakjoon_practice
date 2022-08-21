420cf7c599a54e2baccdb7484b54647d9af33b0c
tjwjdrok <42794396+seojeongrok@users.noreply.github.com>
2022-08-21 16:30:08 +0900

    [Silver II] Title: 싸이버개강총회, Time: 192 ms, Memory: 46716 KB -BaekjoonHub

---
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.py
@@ -0,0 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+S, E, Q = map(str, input().split())
+chat = []
+student = {}
+answer = 0
+
+S = int(S.replace(':', ''))
+E = int(E.replace(':', ''))
+Q = int(Q.replace(':', ''))
+
+cnt = 0
+
+for i in sys.stdin:
+    live = list(i.split())
+
+    enter = int(live[0].replace(':', ''))
+    name = live[1]
+    
+    if enter <= S:
+        student[name] = enter
+    else:
+        if enter >= E and enter <= Q:
+            if name in student and name not in chat:
+                answer += 1
+                chat.append(name)
+
+print(answer)
